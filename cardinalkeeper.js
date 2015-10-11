c6d71e860e40844d78e93285c1d3aeec602a56d6
Khusamov Sukhrob <khusamov@yandex.ru>
2015-10-11 17:32:14 +0000

    Мелкие правки

--- cardinalkeeper.js
+++ cardinalkeeper.js
@@ -2,7 +2,9 @@
 "use strict";
 
 // https://github.com/jsymfony/autoload
-var autoload = require("jsymfony-autoload");
+let autoload = require("jsymfony-autoload");
+
+let fs = require("fs");
 
 /* global CardinalKeeper */
 autoload.register("CardinalKeeper", __dirname + "/library");
@@ -10,6 +12,8 @@ autoload.register("CardinalKeeper", __dirname + "/library");
 var cardinalkeeper = new CardinalKeeper.Application();
 
 cardinalkeeper.publishFolder("/client", __dirname + "/client");
-cardinalkeeper.publishFolder("/vendor", __dirname + "/bower_components");
+
+if (fs.existsSync(__dirname + "/bower_components")) cardinalkeeper.publishFolder("/vendor", __dirname + "/bower_components");
+if (fs.existsSync("./bower_components")) cardinalkeeper.publishFolder("/vendor", "./bower_components");
 
 module.exports = cardinalkeeper;
